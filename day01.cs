dbd3cf7141d20aef8b51556986d3fc6b9ca7b76c
Sharlene Tan <sharlenet@unity3d.com>
2020-12-19 21:35:50 -0800

    Day 1, Star 2

--- day01.cs
+++ day01.cs
@@ -7,15 +7,35 @@ namespace AdventOfCode2015
         static void Main()
         {
             string text = System.IO.File.ReadAllText(@"C:\Users\sharlenet\source\repos\AdventOfCode2015\WriteText.txt");
-            int n = 0;
+            int n = 0, x = 0, y = 0;
+            bool found = false;
+
             foreach (char c in text)
             {
+                x++;
+                
                 if (c == '(')
+                {
                     n++;
+                    if (!found && n == -1)
+                    {
+                        y = x;
+                        found = true;
+                    }
+                }
                 else if (c == ')')
+                {
                     n--;
+                    if (!found && n == -1)
+                    {
+                        y = x;
+                        found = true;
+                    }
+                }
             }
+            
             Console.WriteLine("Result: " + n);
+            Console.WriteLine("Position: " + y);
         }
     }
 }
